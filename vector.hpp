8c463b8e0714f922234e78c6fd7567f9c82a98eb
alvardkhchoyan <alvardkchoyan@gmail.com>
2022-08-04 11:10:15 +0400

    prototypes

---
+++ vector.hpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#ifndef vector
+#define vector
+
+class Vector {
+
+public:
+    Vector();
+    ~Vector();
+
+public:
+    void clear();
+    void shrink_to_fit();
+    int* data();
+//    void resize(int,int);
+    int capacity();
+    void erase(int);
+    int size() const;
+    void insert(int,int);
+    int begin();
+    int end();
+    bool empty();
+    void push_back(int);
+    int at(int);
+    void pop_back();
+
+private:
+    int m_cap;
+    int m_size;
+    int* m_buf;
+};
+
+#endif //vector
